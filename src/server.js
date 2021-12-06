27cd609ed9dd1960494f5ff6d45f6e9cb700858d
Samuel Pérez <samusinho79@gmail.com>
2021-12-06 06:14:30 -0500

    config: nueva ip para servicio de login

--- src/server.js
+++ src/server.js
@@ -1,6 +1,6 @@
 const servers = {
     account_api_url: "https://accountsms.herokuapp.com",
-    auth_api_url: "http://localhost:8000"
+    auth_api_url: "https://auth-ms-p.herokuapp.com"
 }
 
 export default servers;
